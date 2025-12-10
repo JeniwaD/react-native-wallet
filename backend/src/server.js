170319b0c7148358ec22de7dd8d0b4887202cfd1
j <jeniwa@gmail.com>
2025-12-10 15:41:50 +0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import ratelimiter from "./middleware/rateLimiter.js";
+ 
+import transactionsRoute from "./routes/transactionRoute.js";
+import job from "./config/cron.js";
+
+dotenv.config();
+
+const app = express();
+
+if(process.env.NODE_ENV==="production") job.start();
+
+//middleware
+app.use(ratelimiter);
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({status:"ok"})
+});
+
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+    app.listen(5001, () => {
+    console.log("Server is up and running on PORT:", PORT);
+ });
+});
---
+++ mobile/constants/api.js
@@ -0,0 +1,2 @@
+// const API_URL = "https://wallet-api-f47k.onrender.com/api";
+export const API_URL = "http://localhost:5001/api";
